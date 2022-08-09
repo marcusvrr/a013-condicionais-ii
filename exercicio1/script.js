3cf851a83b632f2be2a7ea9d55f178bcdac6cc79
marcusvrr <marcusvrr2@gmail.com>
2022-08-08 21:40:41 -0300

    exer 1 concluido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num = Number(prompt('Digite um número')) //seu numero aqui
+//1
+if(num%2==0){
+    if(num%3==0){
+        console.log('O numero é divisivel por 2 e 3!')
 
+    }else{
+        console.log('O número é divisivel apenas por 2');
+    }
+}else{
+    console.log('O numero não divisivel por 2')
+}
+
+//2
+if(num%2==0 && num%3==0){
+    console.log('O numero é divisivel por 2 e 3!')
+}else{
+    console.log('O numero não divisivel por 2')
+}
